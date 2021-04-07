9af7a1841177b5cc3dfbca3e2df179dc3486f210
Daniel Varón <daniel.varon@mail.escuelaign.edu.co>
2021-04-07 09:38:42 -0500

    json parse

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -9,22 +9,21 @@ var app = (function () {
     }
 
     var generarClima = (function (clima) {
-        var obj = JSON.parse(clima);
         $('#weather').empty();
         var tables;
-        for (let i = 0; i < obj.weather.length; i++) {
-            tables += "<tr> <td>" + obj.weather[i].main + "</td> <td>" + obj.weather[i].description + "</td> </tr>";
+        for (let i = 0; i < clima.weather.length; i++) {
+            tables += "<tr> <td>" + clima.weather[i].main + "</td> <td>" + clima.weather[i].description + "</td> </tr>";
         }
         $('#weather').append(tables)
 
         let temp = document.querySelector('#temp');
-        temp.innerHTML = "Temperature: " + obj.main.temp;
+        temp.innerHTML = "Temperature: " + clima.main.temp;
         let feels_like = document.querySelector('#feels_like');
-        feels_like.innerHTML = "Feels like: " + obj.main.feels_like;
+        feels_like.innerHTML = "Feels like: " + clima.main.feels_like;
         let temp_min = document.querySelector('#temp_min');
-        temp_min.innerHTML = "Low: " + obj.main.temp_min;
+        temp_min.innerHTML = "Low: " + clima.main.temp_min;
         let temp_max = document.querySelector('#temp_max');
-        temp_max.innerHTML = "High: " + obj.main.temp_max;
+        temp_max.innerHTML = "High: " + clima.main.temp_max;
         markers(obj);
 
     })
