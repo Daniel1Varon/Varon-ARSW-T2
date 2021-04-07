178bc5a33a28e0eedc923b396e81715b3e71eee8
Daniel Varón <daniel.varon@mail.escuelaign.edu.co>
2021-04-07 09:24:31 -0500

    json parse

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,38 +1,37 @@
-var app=(function (){
-    var buscar = function (ciudad){
+var app = (function () {
+    var buscar = function (ciudad) {
 
         axios({
             method: 'GET',
-            url: '/weather/'+ciudad
+            url: '/weather/' + ciudad
 
         })
             .then(response => generarClima(response.data))
             .catch(error => console.log(error))
     }
 
-    var generarClima=(function (clima){
+    var generarClima = (function (clima) {
+        var obj = JSON.parse(clima);
         $('#weather').empty();
-        clima.map(function (tmp){
-            var tables;
-            for(let i = 0; i < tmp.weather.length; i++) {
-                tables += "<tr> <td>"+ tmp.weather[i].main +"</td> <td>"+tmp.weather[i].description+"</td> </tr>";
-            }
-            $('#weather').append(tables)
+        var tables;
+        for (let i = 0; i < obj.weather.length; i++) {
+            tables += "<tr> <td>" + obj.weather[i].main + "</td> <td>" + obj.weather[i].description + "</td> </tr>";
+        }
+        $('#weather').append(tables)
 
-            let temp = document.querySelector('#temp');
-            temp.innerHTML = "Temperature: "+ tmp.main.temp;
-            let feels_like = document.querySelector('#feels_like');
-            feels_like.innerHTML = "Feels like: "+ tmp.main.feels_like;
-            let temp_min = document.querySelector('#temp_min');
-            temp_min.innerHTML = "Low: "+ tmp.main.temp_min;
-            let temp_max = document.querySelector('#temp_max');
-            temp_max.innerHTML = "High: "+ tmp.main.temp_max;
-        })
-        markers(clima);
+        let temp = document.querySelector('#temp');
+        temp.innerHTML = "Temperature: " + obj.main.temp;
+        let feels_like = document.querySelector('#feels_like');
+        feels_like.innerHTML = "Feels like: " + obj.main.feels_like;
+        let temp_min = document.querySelector('#temp_min');
+        temp_min.innerHTML = "Low: " + obj.main.temp_min;
+        let temp_max = document.querySelector('#temp_max');
+        temp_max.innerHTML = "High: " + obj.main.temp_max;
+        markers(obj);
 
     })
 
-    return{
-        buscar:buscar
+    return {
+        buscar: buscar
     }
 })();
